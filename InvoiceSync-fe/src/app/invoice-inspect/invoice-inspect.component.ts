d93e1016ca9f9b4bc185e1b03dcf38038fc077a9
janfelber <68186567+janfelber@users.noreply.github.com>
2024-08-11 16:05:46 +0200

    IC-32 Warning ocr
    IC-32 Warning ocr

--- InvoiceSync-fe/src/app/invoice-inspect/invoice-inspect.component.ts
+++ InvoiceSync-fe/src/app/invoice-inspect/invoice-inspect.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InvoiceDisplay } from '../invoice-inspect-display/invoice-inspect-display.component';
+import { InvoiceInspectOcrWarning } from '../invoice-inspect-ocr-warning/invoice-inspect-ocr-warning.component';
 
 @Component({
   selector: 'app-invoice-inspect',
   standalone: true,
-  imports: [InvoiceDisplay],
+  imports: [InvoiceDisplay, InvoiceInspectOcrWarning],
   templateUrl: './invoice-inspect.component.html',
   styleUrl: './invoice-inspect.component.css'
 })
