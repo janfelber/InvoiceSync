ed629c1c17fb2aa599aa608256631614fce24ef4
janfelber <jan.felber.mail@gmail.com>
2024-09-04 15:20:22 +0200

    IS-45-Regexs
    IS-45-Regexs

---
+++ InvoiceSync-be/src/main/java/com/invoicesync/ocr/service/regex/RegexPatterns.java
@@ -0,0 +1,7 @@
+package com.invoicesync.ocr.service.regex;
+
+public class RegexPatterns {
+  // Regular expression for date format DD.MM.YYYY
+  public static final String DATE_DD_MM_YYYY = "\\s*(\\d{1,2}\\.\\d{1,2}\\.\\d{4})";
+
+}
