2db33c343c9fef6dc1655377e9ec1fa0ead22708
Ján Felber <jan.felber.mail@gmail.com>
2024-10-02 18:40:44 +0200

    IS-64 Header improvement
    IS-64 Header improvement

--- InvoiceSync-fe/src/app/header/header.component.ts
+++ InvoiceSync-fe/src/app/header/header.component.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { navbarData } from '../sidenav/nav-data';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,6 +15,17 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  navData = navbarData;
+  currentRoute = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      const currentUrl = this.router.url.slice(1);
+      const currentNav = this.navData.find(nav => nav.routerLink === currentUrl);
+      this.currentRoute = currentNav ? currentNav.label : 'Unknown';
+    });
+  }
 
   // @Input() collapsed = true;
   // @Input() screenWidth = 0;
