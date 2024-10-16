1573373a71a6b82f3c7babb6f6ce47e5b3e178ce
janfelber <jan.felber.mail@gmail.com>
2024-10-16 09:52:06 +0200

    Login implementation
    Login implementation

--- InvoiceSync-be/src/main/java/com/invoicesync/auth/RegisterRequest.java
+++ InvoiceSync-be/src/main/java/com/invoicesync/auth/RegisterRequest.java
@@ -16,4 +16,5 @@ public class RegisterRequest {
     private String password;
     private Role role;
     private String login;
+    private boolean mfaEnabled;
 }
--- InvoiceSync-fe/src/app/app.routes.ts
+++ InvoiceSync-fe/src/app/app.routes.ts
@@ -2,9 +2,22 @@ import { Routes } from '@angular/router';
 import { Invoices } from './invoices/invoices.component';
 import { InvoiceDisplay } from './invoice-inspect-display/invoice-inspect-display.component';
 import { InvoiceInspect } from './invoice-inspect/invoice-inspect.component';
+import {LoginComponent} from "./login/login.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {RegisterComponent} from "./register/register.component";
 
 export const routes: Routes = [
-    {path:'', redirectTo:'invoices', pathMatch: 'full'},
-    {path: 'invoices', component: Invoices},
-    {path: 'test', component: InvoiceInspect},
+    // {path:'', redirectTo:'invoices', pathMatch: 'full'},
+    // {path: 'invoices', component: Invoices},
+    // {path: 'test', component: InvoiceInspect},
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'welcome', component: WelcomeComponent
+  }
+
 ];
