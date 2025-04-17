170b01c99f36efca38a13528964760df5bfb76e0
Ján Felber <jan.felber.mail@gmail.com>
2025-04-17 21:05:20 +0200

    Xml receipt export replaced my Excel document
    Xml receipt export replaced my Excel document

---
+++ InvoiceSync-be/src/main/java/com/invoicesync/controller/ReceiptController.java
@@ -0,0 +1,104 @@
+package com.invoicesync.controller;
+
+import java.util.List;
+
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.invoicesync.dto.receipt.pohoda.ReceiptRequestDTO;
+import com.invoicesync.dto.receipt.reponse.ReceiptDetailsDTO;
+import com.invoicesync.dto.receipt.reponse.ReceiptListDTO;
+import com.invoicesync.repository.CompanyRepository;
+import com.invoicesync.service.InvoiceXmlService;
+import com.invoicesync.service.ReceiptService;
+import com.invoicesync.user.CurrentUserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/receipt")
+public class ReceiptController {
+
+  private final InvoiceXmlService invoiceXmlService;
+  private final CurrentUserService currentUserService;
+
+  private final CompanyRepository companyRepository;
+
+  private final ReceiptService receiptService;
+
+  @PostMapping(value = "/pohoda/export/receipt", produces = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+  public ResponseEntity<byte[]> createReceipt(@RequestBody final ReceiptRequestDTO receiptRequestDTO) {
+    try {
+      final byte[] excel = invoiceXmlService.generatePohodaReceiptXml(receiptRequestDTO);
+
+      final HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(
+          MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+      headers.setContentDisposition(ContentDisposition.attachment().filename("receipt.xlsx").build());
+
+      return new ResponseEntity<>(excel, headers, HttpStatus.OK);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+  }
+
+  //get receipts for current user
+  @GetMapping("/current-user")
+  public List<ReceiptListDTO> getReceiptsCurrentUser() {
+    final Long currentUserId = currentUserService.getCurrentUserId();
+    final List<ReceiptListDTO> receipts = receiptService.getReceiptsByUserId(currentUserId);
+
+    receipts.sort((r1, r2) -> r2.getImportDate().compareTo(r1.getImportDate()));
+
+    return receipts;
+  }
+
+  //get receipts by company id
+  @GetMapping("/company/{id}")
+  public List<ReceiptListDTO> getReceiptsByCompanyId(@PathVariable("id") final Long companyId) {
+    final Long currentUserId = currentUserService.getCurrentUserId();
+
+    if (!companyRepository.existsByIdAndUserId(companyId, currentUserId)) {
+      throw new AccessDeniedException("User does not have access to this company");
+    }
+
+    final List<ReceiptListDTO> receipts = receiptService.getReceiptsByCompanyId(companyId);
+
+    receipts.sort((r1, r2) -> r2.getImportDate().compareTo(r1.getImportDate()));
+
+    return receipts;
+  }
+
+  @GetMapping("/{id}")
+  public ReceiptDetailsDTO getReceiptById(@PathVariable("id") final Long id) {
+    return receiptService.getReceiptById(id);
+  }
+
+  @PostMapping("/save")
+  public void saveReceipt(
+      @RequestParam("file") final MultipartFile file,
+      @RequestParam("companyId") final Long companyId) {
+    final Long currentUserId = currentUserService.getCurrentUserId();
+    receiptService.saveReceipt(file, currentUserId, companyId);
+  }
+
+  @PostMapping("/update/{receiptId}")
+  public void updateReceipt(@PathVariable final Long receiptId,
+      @RequestBody final ReceiptRequestDTO receiptRequestDTO) {
+    final Long currentUserId = currentUserService.getCurrentUserId();
+    receiptService.updateReceiptById(receiptId, currentUserId, receiptRequestDTO);
+  }
+}
---
+++ InvoiceSync-fe/src/app/receipt-details/receipt-details.component.ts
@@ -0,0 +1,176 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {NgClass, NgForOf } from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {AxiosService} from "../axios.service";
+import {ReceiptRequest} from "../models/receipt-request";
+import {InvoiceService} from "../page/invoices/invoice.service";
+import {ToastrService} from "ngx-toastr";
+
+@Component({
+  selector: 'app-receipt-details',
+  imports: [
+    NgForOf,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  templateUrl: './receipt-details.component.html',
+  styleUrl: './receipt-details.component.css'
+})
+export class ReceiptDetailsComponent implements OnInit {
+  constructor(
+    private axiosService: AxiosService,
+    private route: ActivatedRoute,
+    private invoiceService: InvoiceService,
+    private toastr: ToastrService,
+  ) {
+  }
+
+  receiptId: any = null;
+  companyId: any = null;
+  @ViewChild('successToast') successToast!: ElementRef;
+
+
+  receipt: any = {};
+  items: ReceiptRequest['items'] = [];
+  partner: any = {};
+  myIdentity: any = {};
+
+  receiptRequest: ReceiptRequest = {};
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.receiptId = params.get('id') || '';
+    });
+    this.onFetchReceipt();
+  }
+
+  async exportReceipt() {
+    await this.myIdentityCompany();
+
+    this.receiptRequest = {
+      receiptDetails: {
+        numberRequested: 'test',
+        date: this.receipt.date,
+        datePayment: this.receipt.datePayment,
+        dateTax: this.receipt.dateTax,
+        accountValue: this.receipt.accountValue,
+        classificationVAT: this.receipt.classificationVAT,
+        classificationKVVAT: this.receipt.classificationKVVAT,
+        description: this.receipt.description
+      },
+      partner: {
+        name: this.partner.name,
+        city: this.partner.city,
+        street: this.partner.street,
+        zip: this.partner.zip,
+        registrationNumber: this.partner.registrationNumber,
+        taxId: this.partner.taxId,
+        vatId: this.partner.vatId
+      },
+      myIdentity: this.myIdentity,
+      items: this.items
+      };
+
+    console.log(this.receiptRequest)
+
+    this.invoiceService.exportPohodaReceipt(this.receiptRequest).subscribe({
+      next: (response) => {
+        const blob = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'receipt.xlsx'; // <-- zmena z 'receipt.xml'
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+
+        this.toastr.success('Export úspešný', 'Informácia', {
+          timeOut: 1500,
+          progressBar: true,
+          progressAnimation: 'increasing',
+          closeButton: true,
+          positionClass: 'toast-top-right',
+        });
+      },
+      error: (error) => {
+        console.error('Chyba pri exportovaní faktúry', error);
+      }
+    });
+  }
+
+  async updateReceipt() {
+    await this.myIdentityCompany();
+
+    this.receiptRequest = {
+      receiptDetails: {
+        numberRequested: 'test',
+        date: this.receipt.date,
+        datePayment: this.receipt.datePayment,
+        dateTax: this.receipt.dateTax,
+        accountValue: this.receipt.accountValue,
+        classificationVAT: this.receipt.classificationVAT,
+        classificationKVVAT: this.receipt.classificationKVVAT,
+        description: this.receipt.description
+      },
+      partner: {
+        name: this.partner.name,
+        city: this.partner.city,
+        street: this.partner.street,
+        zip: this.partner.zip,
+        registrationNumber: this.partner.registrationNumber,
+        taxId: this.partner.taxId,
+        vatId: this.partner.vatId
+      },
+      items: this.items
+      }
+
+    console.log(this.receiptRequest)
+
+    this.invoiceService.updateReceipt(this.receiptRequest, this.receiptId).subscribe({
+      next: (response) => {
+        console.log(response)
+          this.toastr.success('Bloček úspešne upravený', 'Informácia',
+            {
+              timeOut: 1500,
+              progressBar: true,
+              progressAnimation: 'increasing',
+              closeButton: true,
+              positionClass: 'toast-top-right',
+            });
+      },
+      error: (error) => {
+        console.error('Chyba pri exportovaní faktúry', error);
+      }
+    });
+  }
+
+  async myIdentityCompany() {
+    try {
+      const response = await this.axiosService.request(
+        "GET",
+        `/api/v1/company/info/${this.companyId}`,
+        null
+      );
+      this.myIdentity = response.data;
+    } catch (error) {
+      console.error('Error fetching company info', error);
+    }
+  }
+
+  onFetchReceipt() {
+    this.axiosService.request(
+      'GET',
+      `/api/v1/receipt/${this.receiptId}`,
+      null,
+    ).then(response => {
+      const data = response.data;
+      console.log(data);
+      this.companyId = data.company;
+      this.receipt = data.receiptDetails;
+      this.partner = data.partner;
+      this.items = data.items;
+    });
+  }
+}
