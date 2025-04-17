71afb8ed36befea3be9de7ed99ca62a820c90ef4
Ján Felber <jan.felber.mail@gmail.com>
2025-04-17 21:05:20 +0200

    Xml receipt export replaced my Excel document
    Xml receipt export replaced my Excel document
    
    # Conflicts:
    #	InvoiceSync-be/src/main/java/com/invoicesync/controller/ReceiptController.java
    #	InvoiceSync-fe/src/app/receipt-details/receipt-details.component.ts

--- InvoiceSync-be/src/main/java/com/invoicesync/service/InvoiceXmlServiceImpl.java
+++ InvoiceSync-be/src/main/java/com/invoicesync/service/InvoiceXmlServiceImpl.java
@@ -2,6 +2,8 @@ package com.invoicesync.service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -11,11 +13,16 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 
 import com.invoicesync.dto.invoice.pohoda.InvoiceRequestDTO;
 import com.invoicesync.dto.receipt.pohoda.ReceiptRequestDTO;
+import com.invoicesync.dto.receipt.pohoda.ReceiptRequestDetailsDTO;
 import com.invoicesync.xml.utils.InvoiceXmlHelper;
 
 import lombok.RequiredArgsConstructor;
@@ -51,25 +58,34 @@ public class InvoiceXmlServiceImpl implements InvoiceXmlService{
 
   @Override
   public byte[] generatePohodaReceiptXml(final ReceiptRequestDTO receiptRequestDTO) throws Exception {
-    final File xmlFileReceipt = new File("src/main/resources/template/receipt_template.xml");
-    final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    final Document doc = dBuilder.parse(xmlFileReceipt);
+    final InputStream fis = getClass().getClassLoader().getResourceAsStream("template/receipt_template.xlsx");
+    if (fis == null) {
+      throw new FileNotFoundException("Šablóna receipt_template.xlsx sa nenašla v resources/template/");
+    }
 
-    dbFactory.setNamespaceAware(true);
-    doc.getDocumentElement().normalize();
+    final Workbook workbook = new XSSFWorkbook(fis);
+    final Sheet sheet = workbook.getSheetAt(0);
 
-    xmlHelper.updateReceiptDetails(doc, receiptRequestDTO.getReceiptDetails());
-    xmlHelper.updateReceiptMyIdentity(doc, receiptRequestDTO.getMyIdentity());
-    xmlHelper.updateReceiptPartner(doc, receiptRequestDTO.getPartner());
-    xmlHelper.updateReceiptItems(doc, receiptRequestDTO.getItems());
+    final int newRowIdx = sheet.getLastRowNum() + 1;
+    final Row row = sheet.createRow(newRowIdx);
 
-    final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    final Transformer transformer = TransformerFactory.newInstance().newTransformer();
-    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-    transformer.transform(new DOMSource(doc), new StreamResult(outputStream));
+    final ReceiptRequestDetailsDTO d = receiptRequestDTO.getReceiptDetails();
 
-    return outputStream.toByteArray();
+    row.createCell(0).setCellValue(d.getNumberRequested());
+    row.createCell(1).setCellValue(d.getDate());
+    row.createCell(2).setCellValue(d.getDate());
+    row.createCell(3).setCellValue(d.getDateTax());
+    row.createCell(4).setCellValue(d.getAccountValue());
+    row.createCell(5).setCellValue(d.getClassificationVAT());
+    row.createCell(6).setCellValue(d.getClassificationKVVAT());
+    row.createCell(7).setCellValue(d.getDescription());
+
+    fis.close();
+
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    workbook.write(out);
+    workbook.close();
+    return out.toByteArray();
   }
 
 }
