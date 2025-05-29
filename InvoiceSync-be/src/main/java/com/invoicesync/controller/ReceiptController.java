343969cf470f6ace58eb86d3d7e1684252357a7d
Ján Felber <jan.felber.mail@gmail.com>
2025-05-29 21:00:29 +0200

    Update ReceiptController.java

--- InvoiceSync-be/src/main/java/com/invoicesync/controller/ReceiptController.java
+++ InvoiceSync-be/src/main/java/com/invoicesync/controller/ReceiptController.java
@@ -42,6 +42,7 @@ public class ReceiptController {
   @PostMapping(value = "/export/pohoda", produces = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
   public ResponseEntity<byte[]> createReceipt(@RequestBody final ReceiptRequest request) {
     try {
+      final byte[] excel = invoiceXmlService.generatePohodaReceiptExcel(request);
 
       final HttpHeaders headers = new HttpHeaders();
       headers.setContentType(
