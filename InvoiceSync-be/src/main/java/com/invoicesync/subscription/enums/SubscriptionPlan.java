4f4da564d48a937f2424030bf617ec2e75d6928c
Marcel Felber <marcel.felber.mail@gmail.com>
2025-06-29 21:01:03 +0200

    sub. plan description

--- InvoiceSync-be/src/main/java/com/invoicesync/subscription/enums/SubscriptionPlan.java
+++ InvoiceSync-be/src/main/java/com/invoicesync/subscription/enums/SubscriptionPlan.java
@@ -16,29 +16,32 @@ public enum SubscriptionPlan {
 
   FREE("price_1RLVJ7LJ07OMo5e7HwbkdzmF", FREE_INVOICE_EXPORT_LIMIT, FREE_RECEIPT_EXPORT_LIMIT,
       FREE_INVOICE_CREATE_LIMIT, 0, List.of(
-      "50 faktúr mesačne",
-      "Základné funkcie"
+      "20 spracovaní faktúr mesačne",
+      "20 spracovaní bločkov mesačne",
+      "10 vytvorených faktúr mesačne",
+      "Export (Pohoda, Omega)"
   )),
   ESSENTIALS("price_1RLNmZLJ07OMo5e7zw4IHUEW", ESSENTIALS_INVOICE_EXPORT_LIMIT, ESSENTIALS_RECEIPT_EXPORT_LIMIT,
       ESSENTIALS_INVOICE_CREATE_LIMIT, 6.99, List.of(
-      "150 faktúr mesačne",
-      "Základné funkcie",
-      "Export údajov do Excelu"
+      "150 spracovaní faktúr mesačne",
+      "50 spracovaní bločkov mesačne",
+      "50 vytvorených faktúr mesačne",
+      "Export (Pohoda, Omega)"
   )),
   PRO("price_1RLVHJLJ07OMo5e7lfYnpV4l", PRO_INVOICE_EXPORT_LIMIT, PRO_RECEIPT_EXPORT_LIMIT, PRO_INVOICE_CREATE_LIMIT,
       19.99, List.of(
-      "500 faktúr mesačne",
-      "Export údajov do Excelu",
-      "Automatické ukladanie dát"
+      "500 spracovaní faktúr mesačne",
+      "150 spracovaní bločkov mesačne",
+      "150 vytvorených faktúr mesačne",
+      "Export (Pohoda, Omega)"
   )),
 
-  ENTERPRISE("price_1RLVJ7LJ07OMo5e7RXYZxyz", ENTERPRISE_INVOICE_EXPORT_LIMIT, ENTERPRISE_RECEIPT_EXPORT_LIMIT,
+  ENTERPRISE("price_1RfN9PLJ07OMo5e7LPARcVA1", ENTERPRISE_INVOICE_EXPORT_LIMIT, ENTERPRISE_RECEIPT_EXPORT_LIMIT,
       ENTERPRISE_INVOICE_CREATE_LIMIT, 79.99, List.of(
-      "2000 faktúr mesačne",
-      "API prístup",
-      "Individuálna podpora",
-      "Export údajov do Excelu",
-      "Automatické ukladanie dát"
+      "2000+ spracovaní faktúr mesačne",
+      "1000+ spracovaní bločkov mesačne",
+      "Neobmedzený počet faktúr mesačne",
+      "Export (Pohoda, Omega)"
   ));
 
   private final String priceId;
