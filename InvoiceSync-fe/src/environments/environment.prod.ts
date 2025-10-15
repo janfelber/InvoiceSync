1f0642dd1d3f2dad31c9d435a8d3bd019f9b09ee
Ján Felber <jan.felber.mail@gmail.com>
2025-10-15 16:16:36 +0200

    flyway improvement
    flyway improvement

--- InvoiceSync-fe/src/environments/environment.prod.ts
+++ InvoiceSync-fe/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dex.uctovnictvonitra.sk/api'
+  apiUrl: 'https://dex.uctovnictvonitra.sk/api',
+  stripePublicKey: 'pk_test_51RLMmHLJ07OMo5e7sbeecGQ7cQqTM4Tgg48aI6rP31iTRQXUbH1aVvLRwygvObxwIEjzqiALOR75ojqAm12C9YYk00tjs1CTkq'
 };
