d8d8b440a1b7241b4e7fa9484d1dcc8d321ee3fe
Ján Felber <jan.felber.mail@gmail.com>
2025-10-25 22:34:01 +0200

    xml-file convertor introduced in sidenav
    xml-file convertor introduced in sidenav

--- InvoiceSync-be/src/main/resources/db/sql/upgrade/V2__side_nav_new_routes.sql
+++ InvoiceSync-be/src/main/resources/db/sql/upgrade/V2__side_nav_new_routes.sql
@@ -1,6 +1,11 @@
 
 INSERT INTO invoice_sync.side_nav(id, label, icon, route, parent_id)
 VALUES
+  (1, 'Domov', 'home', '/home', NULL),
+  (2, 'Dokumenty', 'document', NULL, NULL),
+  (3, 'Faktúry', NULL, '/invoices', 2),
+  (4, 'Vystaviť faktúru', NULL, '/invoice/new', 2),
+  (5, 'Bločky', NULL, '/web/receipts', 2),
   (6,'XML Convertor','covertrus','/xml-convertor', NULL),
   (7,'Data Transfer','data_trasnfers','/web/data-transfer', NULL),
   (8,'Subscription','subscribtion','/web/limiter', NULL)
