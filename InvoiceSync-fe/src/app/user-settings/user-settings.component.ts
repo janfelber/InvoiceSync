c5123440e933edf0f5f44d6a71c6bd51708f5681
Ján Felber <jan.felber.mail@gmail.com>
2025-11-01 23:36:24 +0100

    demo profile pics - color background and initials of the name
    demo profile pics - color background and initials of the name

--- InvoiceSync-fe/src/app/user-settings/user-settings.component.ts
+++ InvoiceSync-fe/src/app/user-settings/user-settings.component.ts
@@ -47,5 +47,14 @@ export class UserSettingsComponent implements OnInit {
     });
   }
 
+  getInitials(name: string): string {
+    if (!name) return '';
+    const words = name.trim().split(' ');
+    if (words.length === 1) {
+      return words[0].substring(0, 2).toUpperCase();
+    }
+    return (words[0][0] + words[1][0]).toUpperCase();
+  }
+
 
 }
