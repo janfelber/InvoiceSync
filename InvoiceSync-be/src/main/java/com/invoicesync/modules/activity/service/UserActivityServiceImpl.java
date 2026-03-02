11f62147d2c81b79af36753d268314a772293642
Ján Felber <jan.felber.mail@gmail.com>
2026-03-02 21:03:00 +0100

    IS-145 Set up activity logging infrastructure
    IS-145 Set up activity logging infrastructure

---
+++ InvoiceSync-be/src/main/java/com/invoicesync/modules/activity/service/UserActivityServiceImpl.java
@@ -0,0 +1,8 @@
+package com.invoicesync.modules.activity.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserActivityServiceImpl implements UserActivityService {
+
+}
