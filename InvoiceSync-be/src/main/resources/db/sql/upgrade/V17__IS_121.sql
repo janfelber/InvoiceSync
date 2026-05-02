7c65f3b75e53b62667cd0a72e7b9b362fa01a177
Ján Felber <jan.felber.mail@gmail.com>
2026-05-02 16:22:39 +0200

    IS-121 bugfix
    IS-121 - missing renamed db columns

--- InvoiceSync-be/src/main/resources/db/sql/upgrade/V17__IS_121.sql
+++ InvoiceSync-be/src/main/resources/db/sql/upgrade/V17__IS_121.sql
@@ -6,3 +6,39 @@ ALTER TABLE invoice_sync.receipt
 
 ALTER TABLE invoice_sync.receipt
   RENAME COLUMN cash_receipt_number TO receipt_number;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN date_payment TO payment_date;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN date_tax TO tax_date;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN accounting TO account_value;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN classification_vat TO vat_classification;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN classification_kv_vat TO kv_vat_classification;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN partner_name TO supplier_name;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN partner_city TO supplier_city;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN partner_street TO supplier_street;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN partner_zip TO supplier_zip;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN partner_registration_number TO supplier_registration_number;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN partner_tax_id TO supplier_tax_id;
+
+ALTER TABLE invoice_sync.receipt
+  RENAME COLUMN partner_vat_id TO supplier_vat_id;
