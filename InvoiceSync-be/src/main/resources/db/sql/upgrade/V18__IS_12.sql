b5322fde866f373e0704273fd1768acf541489fa
Ján Felber <jan.felber.mail@gmail.com>
2026-08-05 22:04:43 +0200

    IS-12 Values from PDF invoice - fix duplicated migration file

--- InvoiceSync-be/src/main/resources/db/sql/upgrade/V18__IS_12.sql
+++
@@ -1,4 +0,0 @@
--- add new columns to invoice - total price without vat and with vat
-ALTER TABLE invoice_sync.invoice
-  ADD COLUMN total_price_without_vat NUMERIC(12, 2),
-  ADD COLUMN total_price_with_vat    NUMERIC(12, 2);
